125e1126806edead31645af086ac9b0a5abded96
Daniel Dixon <djd41@kent.ac.uk>
2026-03-04 21:06:05 +0000

    feat(calendar) inital setup for calendar component

---
+++ src/components/pages/Calendar.jsx
@@ -0,0 +1,17 @@
+/*
+ * Home Page Component
+ *
+ * Renders the content of the home page
+ *
+ * @component
+ * @returns {JSX.Element} The Home page component
+ *
+ * Last updated: 21/02/2026
+*/
+
+
+export default function Calendar() {
+    return (
+        <h1>Calendar</h1>
+    )
+}
