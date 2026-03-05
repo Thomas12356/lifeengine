a117546cc857ee77df4a41e9c7b956345d134731
Thomas Eardley <te215@kent.ac.uk>
2026-03-05 17:01:10 +0000

    feat(navbar): Added styling for NavLinks. Added styling for NavBar. Moved navlink contents to array. Moved styling to variables.

--- src/components/layouts/NavBar.jsx
+++ src/components/layouts/NavBar.jsx
@@ -1,56 +1,93 @@
+{/* -------------------- Imports -------------------- */}
+{/* Chakra UI */}
 import { Link as ChakraLink, Icon as ChakraIcon, Text, Box, Flex, HStack } from "@chakra-ui/react";
+{/* Router */}
 import { NavLink } from "react-router-dom";
-//components
+{/* UI Components */}
 import LifeEngineLogo from "@ui-components/LifeEngineLogo";
-//test icons
+{/* Assets */}
+
+{/* Temporary Dev Imports */}
 import { FaBeer } from "react-icons/fa";
 
-//TODO style logo properly 
 
-
-const NavLinkStyles = {
+{/* -------------------- Styling -------------------- */}
+{/* NavLink Styles */}
+const navLinkStyles = {
     display: "flex",
-    alignItems:"center"
+    alignItems:"center",
+    marginRight: "general.smSpacing",
+}
+const navLinkTextStyles = {
+    fontSize: "sm",
+    marginLeft: "general.xsSpacing",
+}
+const navBarIconStyles = {
+    boxSize: "20px",
+}
+{/* NavBar Styles */}
+const navBarBoxStyles = {
+    bg: "white",
+    borderRadius: "widgetRadii",
+    marginLeft: "widget.mLeftRight",
+    marginRight: "widget.mLeftRight",
+    marginTop: "widget.mTop",
+    paddingLeft: "widget.pLeftRight",
+    paddingRight: "widget.pLeftRight",
+    paddingTop: "widget.pTopBottom",
+    paddingBottom: "widget.pTopBottom"
 }
 
-const NavButtons = [
-  { to: "/", Icon: FaBeer, text: "Home" },
-  { to: "/calendar", Icon: FaBeer, text: "Calendar" },
+
+{/* -------------------- Config -------------------- */}
+const navButtons = [
+    { to: "/", Icon: FaBeer, text: "Home" },
+    { to: "/calendar", Icon: FaBeer, text: "Calendar" },
+    { to: "/habits", Icon: FaBeer, text: "Habits" },
+    { to: "/insights", Icon: FaBeer, text: "Insights" },
+    { to: "/agent", Icon: FaBeer, text: "Agent" },
+    { to: "/profile", Icon: FaBeer, text: "Profile" },
+    { to: "/social", Icon: FaBeer, text: "Social" },
 ];
 
+{/* -------------------- Local Components -------------------- */}
 const NavButton = ({ to, Icon, text}) => {
     return(
-        <NavLink to={to} style={{ textDecoration: 'none', display: 'block' }}>
+        <NavLink to={to} style={{ textDecoration: 'none', display: 'block', ...navLinkStyles }}>
             {({ isActive }) => (
                 <Box
-                    {...NavLinkStyles}
+                    {...navLinkStyles}
                     color={isActive ? "brand.blueLight" : "brand.gray"}
                 >
                     {/* TODO: icon size is hardcoded for now, will need to be changed when we have real icons */}
-                    <ChakraIcon as={Icon} boxSize="20px" />
+                    <ChakraIcon as={Icon} {...navBarIconStyles} />
                     {/* TODO: font size is hardcoded for now, will need to be changed when we have real text */}
-                    <Text fontSize = "sm">{text}</Text>
+                    <Text {...navLinkTextStyles}>{text}</Text>
                 </Box>
             )}
         </NavLink>
     )
 }
 
+{/* -------------------- Main Component -------------------- */}
 export default function NavBar() {
     return(
-        <Box bg="white" borderRadius="widgetRadii" marginLeft="MWidgetLeftRight" marginRight="MWidgetLeftRight" marginTop="MWidgetTop" paddingLeft="PWidgetLeftRight" paddingRight="PWidgetLeftRight" paddingTop="PWidgetTopBottom" paddingBottom="PWidgetTopBottom">
+        <Box {...navBarBoxStyles}>
             <Flex justifyContent="space-between" alignItems="center">
 
-                <LifeEngineLogo />
+                <HStack>
+                    <LifeEngineLogo />
 
-                <nav>
-                    {/* TODO: Style the nav buttons, currently just using the same icon for all of them */}
-                    <HStack>
-                        {NavButtons.map((button) => (
-                            <NavButton key={button.to} {...button} />
-                        ))}
-                    </HStack>
-                </nav>
+                    <nav>
+                        <HStack>
+                            {navButtons.map((button) => (
+                                <NavButton key={button.to} {...button} />
+                            ))}
+                        </HStack>
+                    </nav>
+                </HStack>
+
+                <Text>TODO: Notifications and Settings</Text>
 
             </Flex>
         </Box>
