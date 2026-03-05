8dc60075e017867fb8a5551b6ccb66ec714bc4e5
Dan Dixon <dandixontab119@gmail.com>
2026-03-05 17:22:29 +0000

    feat(calendar) : created calendar header layout

--- src/components/ui-components/calendar/sub-components/CalendarHeader.jsx
+++ src/components/ui-components/calendar/sub-components/CalendarHeader.jsx
@@ -1,7 +1,25 @@
 
+import { HStack, VStack, Text } from "@chakra-ui/react"
+
+const CalendarHeaderDate = ({ weekday, date }) => {
+    return (
+        <VStack>
+            <Text>{weekday}</Text>
+            <Text>{date}</Text>
+        </VStack>
+    )
+}
 
 export default function CalendarHeader() {
     return (
-        <h1>Calendar Header</h1>
+        <HStack>
+            <CalendarHeaderDate weekday="Mon" date="1" />
+            <CalendarHeaderDate weekday="Tue" date="2" />
+            <CalendarHeaderDate weekday="Wed" date="3" />
+            <CalendarHeaderDate weekday="Thu" date="4" />
+            <CalendarHeaderDate weekday="Fri" date="5" />
+            <CalendarHeaderDate weekday="Sat" date="6" />
+            <CalendarHeaderDate weekday="Sun" date="7" />
+         </HStack>
     )
 }
