c54a3759c20adea0df00cc9af744c0fa26112111
Dan Dixon <dandixontab119@gmail.com>
2026-03-05 18:41:07 +0000

    refactor(calendar body) : cleaned up code

--- src/components/ui-components/calendar/sub-components/CalendarHeader.jsx
+++ src/components/ui-components/calendar/sub-components/CalendarHeader.jsx
@@ -1,9 +1,9 @@
 
-import { HStack, VStack, Text } from "@chakra-ui/react"
+import { HStack, VStack, Text, Box } from "@chakra-ui/react"
 
 const CalendarHeaderDate = ({ weekday, date }) => {
     return (
-        <VStack>
+        <VStack flex="1">
             <Text>{weekday}</Text>
             <Text>{date}</Text>
         </VStack>
@@ -11,15 +11,23 @@ const CalendarHeaderDate = ({ weekday, date }) => {
 }
 
 export default function CalendarHeader() {
+
+    const days = [
+        { weekday: "Mon", date: "1" },
+        { weekday: "Tue", date: "2" },
+        { weekday: "Wed", date: "3" },
+        { weekday: "Thu", date: "4" },
+        { weekday: "Fri", date: "5" },
+        { weekday: "Sat", date: "6" },
+        { weekday: "Sun", date: "7" },
+    ]
+
     return (
-        <HStack>
-            <CalendarHeaderDate weekday="Mon" date="1" />
-            <CalendarHeaderDate weekday="Tue" date="2" />
-            <CalendarHeaderDate weekday="Wed" date="3" />
-            <CalendarHeaderDate weekday="Thu" date="4" />
-            <CalendarHeaderDate weekday="Fri" date="5" />
-            <CalendarHeaderDate weekday="Sat" date="6" />
-            <CalendarHeaderDate weekday="Sun" date="7" />
+        <HStack w="100%" spacing={0}>
+            <Box w="60px"/>
+            {days.map((day, index) => (
+                <CalendarHeaderDate key={index} weekday={day.weekday} date={day.date} />
+            ))}
          </HStack>
     )
 }
