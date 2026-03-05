69eaf9edd361e55cd80e12ec58a9372a0224649f
Thomas Eardley <te215@kent.ac.uk>
2026-03-05 13:53:58 +0000

    feat(app): styling of app background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 //-----Imports-----//
 import { Provider } from "@/components/ui/provider"
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Box } from "@chakra-ui/react"
 //Components
 import NavBar from "@layouts/NavBar"
 //Pages
@@ -10,11 +11,21 @@ export default function App() {
     return (
         <Provider>
             <BrowserRouter>
-            <NavBar />
+            <Box
+                position="fixed"
+                top="0"
+                left="0"
+                w="100vw"
+                h="100vh"
+                bg="radial-gradient(circle, white, #EDEDED)"
+                zIndex="-1"
+            >
+                <NavBar />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/calendar" element={<h1>Calendar</h1>} />
                 </Routes>
+            </Box>
             </BrowserRouter>
         </Provider>
     )
