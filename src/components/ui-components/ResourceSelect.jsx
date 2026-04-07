f23a33d11e6d87e6f29fc433b7909f75b2735a33
Thomas Eardley <te215@kent.ac.uk>
2026-04-07 15:11:08 +0100

    refactor(select): renamed imort for clarity to ChakraSelect

--- src/components/ui-components/ResourceSelect.jsx
+++ src/components/ui-components/ResourceSelect.jsx
@@ -1,17 +1,18 @@
 import { HStack, Text } from "@chakra-ui/react"
-import { Select } from "@chakra-ui/select"
+import { Select as ChakraSelect } from "@chakra-ui/select"
 
 export default function ResourceSelect({ resource, options = ['Low', "Normal", "High"] }) {
     return(
         <HStack>
             <Text>{resource}:</Text>
-            <Select icon={<></>}>
+            {/* Change to menu ?? */}
+            <ChakraSelect icon = {<></>}>
                 {options.map((opt) => (
                     <option key={opt} value={opt}>
                         {opt}
                     </option>
                 ))}
-            </Select>
+            </ChakraSelect>
         </HStack>
     )
 }
