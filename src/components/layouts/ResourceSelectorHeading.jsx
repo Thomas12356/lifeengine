a274f5d41e55501283d2ff9d61b5e863662baa54
Thomas Eardley <te215@kent.ac.uk>
2026-04-13 16:21:49 +0100

    wip(resourceselectorheading): added user greeting

--- src/components/layouts/ResourceSelectorHeading.jsx
+++ src/components/layouts/ResourceSelectorHeading.jsx
@@ -1,11 +1,36 @@
-import ResourceSelect from "@ui-components/ResourceSelect"
-import { HStack } from "@chakra-ui/react";
+/**
+ * Resource Selector Heading Layout
+ *
+ * Displays multiple resource selectors and a greeting message to the user.
+ *
+ * @component
+ * @returns {JSX.Element} ResourceSelectorHeading layout
+ *
+ * Last updated: 13/04/2026 by te215@kent.ac.uk
+ * 
+ * TODO:
+ * - Add time-based greetings
+ * - Add hoverable tooltips to explain the purpose the resource selector.
+ * - Add Styling and Theming
+ */
 
-export default function ResourceSelectorHeading() {
+import ResourceSelect from "@ui-components/ResourceSelect"
+import { HStack, VStack, Text, Stack } from "@chakra-ui/react";
+
+export default function ResourceSelectorHeading({ username }) {
     return(
         <HStack>
-            <ResourceSelect resource="Focus" />
-            <ResourceSelect resource="Energy" />
+
+            <Stack gap={0}>
+                <Text textStyle="headingSolid">Hello, {username}!</Text> {/* TODO - Good morning and Good evening messages instead of Hello */}
+                <Text>How are you feeling today?</Text> {/* TODO - Add Hoverable Tool Tip */}
+            </Stack>
+
+            <HStack>
+                <ResourceSelect resource="Focus" />
+                <ResourceSelect resource="Energy" />
+            </HStack>
+        
         </HStack>
     )
 }
