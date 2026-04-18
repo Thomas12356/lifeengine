f40a0d2faed7b3480a24eb77846c94422b36f8b5
Thomas Eardley <te215@kent.ac.uk>
2026-04-18 18:45:26 +0100

    style(WidgetBox): Added styling to the widget box

--- src/components/ui-components/WidgetBox.jsx
+++ src/components/ui-components/WidgetBox.jsx
@@ -4,6 +4,14 @@ export function WidgetBox({ children, ...props }){
     return(
         <Box
         bg={"brand.white"}
+        paddingLeft={"widget.pLeftRight"}
+        paddingRight={"widget.pLeftRight"}
+        paddingTop={"widget.pTopBottom"}
+        paddingBottom={"widget.pTopBottom"}
+        borderRadius={"widgetRadii"}
+        ml={"widget.mLeftRight"}
+        mr={"widget.mLeftRight"}
+        mt={"widget.mTopBottom"}
         {...props}>
             {children}
         </Box>
