699c2c7744118749c66d79ac403d22fb93d0f13e
djd41@kent.ac.uk <dandixontab119@gmail.com>
2026-04-18 13:32:18 +0100

    fix(calendar page) : restored file deleted during rebase

---
+++ src/pages/CalendarPage.jsx
@@ -0,0 +1,18 @@
+/*
+ * Calendar Page Component
+ *
+ * Renders the content of the calendar page
+ *
+ * @component
+ * @returns {JSX.Element} The Calendar page component
+ *
+ * Last updated: 21/02/2026
+*/
+
+import CalendarLayout from "@layouts/CalendarLayout";
+
+export default function CalendarPage() {
+    return (
+        <CalendarLayout />
+    )
+}
