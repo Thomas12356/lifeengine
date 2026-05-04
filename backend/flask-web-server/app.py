7417a76494d2dca94a6c51d60c55d5aac6f2411d
Thomas Eardley <te215@kent.ac.uk>
2026-05-04 17:21:01 +0100

    docs(flask_app): added comments

--- backend/flask-web-server/app.py
+++ backend/flask-web-server/app.py
@@ -1,6 +1,13 @@
+"""
+Main entry point for flask app.
+Run this file to start the server.
+"""
+
 from api import create_app
 
+# Create flask app using factory function.
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Start the server, host = 0.0.0.0 to allow local network access.
+    app.run(debug=True, port=5000)
