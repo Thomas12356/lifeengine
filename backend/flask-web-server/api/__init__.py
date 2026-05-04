3c4986867ef9b99aac061b214c1c8c696f11d24f
Thomas Eardley <te215@kent.ac.uk>
2026-05-04 17:23:31 +0100

    docs(flask-factory): added comments

--- backend/flask-web-server/api/__init__.py
+++ backend/flask-web-server/api/__init__.py
@@ -1,12 +1,26 @@
+"""
+Backend API package initialization.
+Sets up Flask app and registers Blueprints for the API routes.
+"""
+
+# ? should models be global as we want to make the shcedular modular and not have it depend on the flask app?
+
 from flask import Flask
 
 def create_app():
-    # Create flask application
+    """
+    App factory: Creates and configures the Flask appinstance.
+    """
     app = Flask(__name__)
+
+    # TODO Security: handle secret key properly for production.
     app.config['SECRET_KEY'] = 'life-engine-secret-key'
 
-    # Import Blueprints
+    # TODO Should We: use CORS, react usually port 3000, flask usually port 5000.
 
-    # Register Blueprints
+    # TODO Import Blueprints
+
+    # TODO Register Blueprints
+
 
     return app
