1c38ad16ddfe34c989ecba361e201ef654066281
Thomas Eardley <te215@kent.ac.uk>
2026-05-04 14:41:41 +0100

    feat(vite-config): initialised proxy for flask webserver api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,13 @@ import jsconfigPaths from 'vite-jsconfig-paths'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
