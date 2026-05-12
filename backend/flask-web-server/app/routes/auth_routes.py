03654b990a2b58fe2f04b128d6d129c5738fa5f0
Thomas Eardley <te215@kent.ac.uk>
2026-05-12 19:53:25 +0100

    refactor(auth routes flask): remove unecisary import

--- backend/flask-web-server/app/routes/auth_routes.py
+++ backend/flask-web-server/app/routes/auth_routes.py
@@ -7,7 +7,6 @@ Authentication blueprint
 
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
-from app import db
 from app.services.auth_services import register_user, authenticate_user
 
 auth_blueprint = Blueprint('auth', __name__)
@@ -18,8 +17,8 @@ def login():
     Validates user credentials
     """
     data = request.get_json()
-
     user = authenticate_user(data.get('email'), data.get('password'))
+    
     
     if not user:
         return jsonify({"error": "Invalid email or password."}), 401
