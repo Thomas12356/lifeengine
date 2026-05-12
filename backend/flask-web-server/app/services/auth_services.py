31003ed3f2d0eb7c1288669730aa198de616e839
Thomas Eardley <te215@kent.ac.uk>
2026-05-12 15:49:37 +0100

    docs(auth services): added comments for register user

--- backend/flask-web-server/app/services/auth_services.py
+++ backend/flask-web-server/app/services/auth_services.py
@@ -2,6 +2,7 @@ from app import db
 from app.models import User
 from app.utils import verify_password, generate_new_hash
 
+
 def register_user(email, password, first_name, last_name):
     """
     Registers a new user with the given email, password, first name and last name.
@@ -12,6 +13,7 @@ def register_user(email, password, first_name, last_name):
     
     password_hash, salt = generate_new_hash(password)
 
+    # Create a new user instance and save to the database
     new_user = User(
         email=email,
         password_hash=password_hash,
@@ -20,6 +22,7 @@ def register_user(email, password, first_name, last_name):
         last_name=last_name
     )
 
+    # Add the new user to the database session and commit
     db.session.add(new_user)
     db.session.commit()
 
