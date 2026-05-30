82eb8ee42d9a9b9e182067be010dc5197cb17306
Thomas eardley <te215@kent.ac.uk>
2026-05-30 18:25:41 +0100

    wip(agent): handle a user_message and run with test file

--- services/agent/agent.py
+++ services/agent/agent.py
@@ -2,11 +2,13 @@ import json
 import os
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
+from ollama import Client
 
 load_dotenv()
 
 MODEL = os.getenv("MODEL")
 OLLAMA_HOST = os.getenv("OLLAMA_HOST")
+client = Client(host=OLLAMA_HOST)
 
 SYSTEM_PROMPT = """
 You are an assistant for a calendar app called Ellie.
@@ -34,3 +36,59 @@ If the user is just chatting, return:
   "message": "your response"
 }
 """
+
+def ask_llm(user_message: str) -> dict:
+    """
+        Send users message to Ollama
+        Expecting JSON from request.
+    """
+
+    response = client.chat(
+        model=MODEL,
+        messages=[
+            {
+                "role" : "system",
+                "content" : SYSTEM_PROMPT
+            },
+            {
+                "role" : "user",
+                "content" : user_message,
+            }
+        ],
+        format="json",
+        options={"temperature" : 0},
+    )
+
+    content = response["message"]["content"]
+    
+    try:
+        return json.loads(content)
+    except json.JSONDecodeError:
+        return {
+            "action" : "chat",
+            "message" : "Sorry, I could not understand that.",
+        }
+    
+def add_event(title, date_text, start_text, end_text):
+    return "function reached"
+
+def handle_message_from_user(user_message: str):
+    llm_result = ask_llm(user_message)
+    action = llm_result.get("action")
+
+    if action == "add_event":
+        return add_event(
+            title=llm_result.get("title"),
+            date_text=llm_result.get("date_text"),
+            start_text=llm_result.get("start_text"),
+            end_text=llm_result.get("end_text"),
+        )
+    
+    if action == "chat":
+        return {
+            "message" : llm_result.get("message", ""),
+        }
+    
+    return{
+        "message" : "Sorry, I did not understant that."
+    }
