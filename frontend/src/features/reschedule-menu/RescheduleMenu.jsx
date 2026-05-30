7f9608f13291f31c42706297d7e9bb862c8a8c4b
djd41@kent.ac.uk <dandixontab119@gmail.com>
2026-05-30 18:09:10 +0100

    feat(RescheduleMenu) : Added state form handling and submission

--- frontend/src/features/reschedule-menu/RescheduleMenu.jsx
+++ frontend/src/features/reschedule-menu/RescheduleMenu.jsx
@@ -1,9 +1,62 @@
+import { autoReschedule, rescheduleEvent } from "@/utils/eventServices";
 import { Button, Dialog, CloseButton, Portal, Stack, Input, Field, Text} from "@chakra-ui/react";
+import { useState, useEffect } from "react"
+import buildReschedulePayload from "./utils/buildReschedulePayload";
 
 
 export default function RescheduleMenu({ isOpen, onOpenChange, event }) {
 
-    console.log(event)
+    const [formData, setFormData] = useState({
+        newDate : "",
+        newStart : "",
+        newEnd : "",
+        eventID : event.id
+    })
+
+    function updateField(field, value) {
+        setFormData((prev) => ({
+            ...prev,
+            [field] : value,
+        }))
+    }
+
+    async function handleReschedule() {
+        try {
+            const payload = buildReschedulePayload(formData)
+            await rescheduleEvent(payload)
+
+            setFormData({
+                newData : "",
+                newStart: "",
+                newEnd : ""
+            })
+
+            onOpenChange(false)
+        } catch (err) {
+            console.log("Failed to reschedule event:", err)
+        }
+    }
+
+    async function handleAutoReschedule() {
+        try {
+            const payload = buildReschedulePayload(formData, true)
+            await autoReschedule(payload)
+        } catch (err) {
+            console.log("Failed to auto-reschedule event:", err)
+        }
+    }
+
+    useEffect(() => {
+        if (!event) return
+        
+        setFormData({
+            newDate : "",
+            newStart: "",
+            newEnd : "",
+            eventID : event.id
+        })
+    }, [isOpen, event.id])
+
     const autoReschedulable = event?.is_moveable
 
     return (
@@ -23,30 +76,43 @@ export default function RescheduleMenu({ isOpen, onOpenChange, event }) {
                         <Dialog.Body>
                             <Stack>
                                 <Field.Root>
+                                    <Field.Label>
+                                        New date
+                                    </Field.Label>
+                                    <Input 
+                                        type="date"
+                                        value={formData.date}
+                                        onChange={(e) => updateField("newDate", e.target.value)}
+                                    />
+                                </Field.Root>
+                                <Field.Root>
                                     <Field.Label>New start time</Field.Label>
-                                    <Input type="time" />
+                                    <Input 
+                                        type="time"
+                                        value={formData.newStart}
+                                        onChange={(e) => updateField("newStart", e.target.value)}
+                                    />
                                 </Field.Root>
                                 <Field.Root>
                                     <Field.Label>New end time</Field.Label>
-                                    <Input type="time" />
+                                    <Input 
+                                        type="time"
+                                        value={formData.newEnd}
+                                        onChange={(e) => updateField("newEnd", e.target.value)}
+                                    />
                                 </Field.Root>
-                                <Button disabled={!autoReschedulable}>
-                                    Auto reschedule
-                                </Button>
-                                {!autoReschedulable && (
-                                    <Text textStyle="defaultText" fontSize="sm">
-                                        To use this feature, allow auto rescheduling for this event.
-                                    </Text>
-                                )}
                             </Stack>
                         </Dialog.Body>
                         <Dialog.Footer>
-                            <Dialog.ActionTrigger asChild>
-                                <Button>
-                                    Cancel
-                                </Button>
-                            </Dialog.ActionTrigger>
-                            <Button>
+                            <Button disabled={!autoReschedulable} onClick={handleAutoReschedule}>
+                                Auto reschedule
+                            </Button>
+                            {!autoReschedulable && (
+                                <Text textStyle="defaultText" fontSize="sm">
+                                    To use this feature, allow auto rescheduling for this event.
+                                </Text>
+                            )}
+                            <Button onClick={handleReschedule}>
                                 Reschedule
                             </Button>
                         </Dialog.Footer>
