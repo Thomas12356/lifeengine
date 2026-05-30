3a797be294c9be5d93542f232aa1b70c3387ca9e
Dan Dixon <djd41@kent.ac.uk>
2026-05-30 01:41:59 +0100

    feat(ScheduleWidget) : Replaced local dummy data with fetch from useHompage hook

--- frontend/src/features/schedule-widget/ScheduleWidget.jsx
+++ frontend/src/features/schedule-widget/ScheduleWidget.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { Box, Text, VStack, HStack } from '@chakra-ui/react';
 import { WidgetBox } from '@ui-components/WidgetBox';
+import { useHomepage } from '@/context/HomepageContext';
 //import { fetchEvents } from "@utils/eventServices"
 
 export default function ScheduleWidget() {
-  const scheduleItems = [
-    { time: '11:00 AM', event: 'Meet Liam about Project' },
-    { time: '12:00 PM', event: 'Food Shop' },
-    { time: '01:00 PM', event: 'Golf Range' },
-    { time: '02:00 PM', event: 'University Lecture' },
-    { time: '02:00 PM', event: 'University Lecture' },
-    { time: '02:00 PM', event: 'University Lecture' },
-  ];
-
-  const todaysDate = new Date();
+  const { todaysEvents } = useHomepage()
 
   return (
     <WidgetBox>
@@ -42,15 +34,15 @@ export default function ScheduleWidget() {
                 borderRadius: "4px",},
             }}>
 
-          {scheduleItems.map((item, index) => (
-            <HStack key={index} spaceX="6" align="baseline">
+          {todaysEvents.map((event) => (
+            <HStack key={event.id} spaceX="6" align="baseline">
 
               <Text textStyle="darkBlueText">
-                {item.time}
+                {event.start_time}
               </Text>
 
               <Text textStyle="defaultText">
-                {item.event}
+                {event.name}
               </Text>
 
             </HStack>
